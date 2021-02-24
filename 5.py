07db20b4337b88ddacbd10a34145e2d7d0c7dc3d
JohnnyBoomKek <summer1710@qq.com>
2021-02-24 15:32:44 +0800

    5th

---
+++ 5.py
@@ -0,0 +1,69 @@
+import socket 
+from select import select
+
+tasks = []
+to_read = {}
+to_write = {}
+
+def server():
+    #init server socket
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind(('localhost', 1337))
+    server_socket.listen()
+    
+    while True:
+        #gives back control to the event loop
+        yield ('read', server_socket)
+
+        client_socket, addr = server_socket.accept()
+
+        print("Connection from", addr)
+
+        tasks.append(client(client_socket))
+
+def client(client_socket):
+    while True:
+
+        yield ('read', client_socket)
+        request = client_socket.recv(4096)
+
+        if not request:
+            break
+        else:
+            response = 'hello world \n '.encode()
+
+            yield ('write', client_socket)
+            client_socket.send(response)
+
+    client_socket.close()
+
+
+
+def event_loop():
+
+    while any([tasks, to_read, to_write]):
+
+        while not tasks:
+            ready_to_read, ready_to_write, _ = select(to_read, to_write, [])
+
+            for soc in ready_to_read:
+                tasks.append(to_read.pop(soc))
+            
+            for soc in ready_to_write:
+                tasks.append(to_write.pop(soc))
+
+        try: 
+            task = tasks.pop(0)
+
+            identifier, soc = next(task)
+
+            if identifier == 'read':
+                to_read[soc] = task
+            if identifier == 'write':
+                to_write[soc] = task
+        except StopIteration:
+            print('Done!')
+
+tasks.append(server())
+event_loop()
